8960f40d32f3ecdb61fc9e0dd3a3137f9da35ac6
Nikunj Handa <nikunj.sg@gmail.com>
2013-08-24 15:38:51 +0000

    Initial

---
+++ HelloGhana.java
@@ -0,0 +1,5 @@
+class HelloGhana extends Parent implements Interface {
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld extends Parent implements Interface {
+	public static void main(String[] args) {
+		System.out.print("Hello World!");
+	}
+}
